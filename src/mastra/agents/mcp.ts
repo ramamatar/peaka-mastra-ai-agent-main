7201cb09f37e513dd636c38934410b9a7acb62c8
ramamatar <166447147+ramamatar@users.noreply.github.com>
2025-07-01 13:53:44 +0300

    Update mcp.ts

--- src/mastra/agents/mcp.ts
+++ src/mastra/agents/mcp.ts
@@ -1,5 +1,8 @@
 import { MCPClient } from "@mastra/mcp";
 
+process.env.HOME = "/tmp";
+console.log("PEAKA_API_KEY" + process.env.PEAKA_API_KEY);
+
 export const mcp = new MCPClient({
   servers: {
     peaka: {
